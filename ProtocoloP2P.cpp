7ddf479a796b834eb1cd1f7aac2584fd3aed3352
Mauricio <Mauricio@6ba988c5-0224-0410-acf2-7d9395eead91>
2005-11-28 22:09:15 +0000

    Movido ProtocoloP2P.cpp, melhoramentos no tratamento dos arquivos
    git-svn-id: https://mbsutil.googlecode.com/svn@33 6ba988c5-0224-0410-acf2-7d9395eead91

---
+++ ProtocoloP2P.cpp
@@ -0,0 +1,951 @@
+#include "ProtocoloP2P.h"
+#include <ctime>
+#include <ios>
+#include <fstream>
+
+//#define LOGAR_COMANDOS
+//#define LOGAR_SOCKET
+
+using namespace std;
+using namespace Protocolo;
+
+namespace
+{
+	const int TAMBUFFER=500;
+	ClienteP2PUI *p2pui=NULL;
+	Mutex mutexMostrarMsg;
+	Mutex mutexLogar;
+	void mostrarMsg(Hash128 *user, string frase)
+	{
+		mutexMostrarMsg.trava();
+		if(p2pui!=NULL)
+			p2pui->mostraMensagemChat(user,frase);
+		else
+		{
+			cerr << "ClienteP2PUI::mostraMensagemChat(" << user->toString();
+			cerr << ", " << frase << ")" << endl;
+		}
+		mutexMostrarMsg.destrava();
+	}
+	void logar(string frase)
+	{
+		mutexLogar.trava();
+		if(p2pui!=NULL)
+			p2pui->relatorio(frase);
+		else
+			cerr << "ClienteP2PUI::relatorio(" << frase << ")" << endl;
+		mutexLogar.destrava();
+	}
+	typedef struct _CabecalhoLVL0
+	{
+		unsigned short tam;
+		unsigned short com;
+	} CabecalhoLVL0;
+};
+
+//------------------------------------------------------------------------------
+//      Noh
+//------------------------------------------------------------------------------
+
+Noh::Noh()
+{
+    ip=id=0;
+    porta=PORTA_PADRAO;
+}
+
+int Noh::read(unsigned char *&pkt)
+{
+    ip=*((unsigned long*)pkt);
+    pkt+=sizeof(ip);
+    porta=*((unsigned short*)pkt);
+    pkt+=sizeof(porta);
+    id=*((unsigned long*)pkt);
+    pkt+=sizeof(id);
+    return 0;
+}
+
+int Noh::write(unsigned char *&pkt) const
+{
+    *((unsigned long*)pkt)=ip;
+    pkt+=sizeof(ip);
+    *((unsigned short*)pkt)=porta;
+    pkt+=sizeof(porta);
+    *((unsigned long*)pkt)=id;
+    pkt+=sizeof(id);
+    return 0;
+}
+
+std::istream& Noh::read(std::istream& is)
+{
+	is.read((char*)&ip,sizeof(ip));
+	is.read((char*)&porta,sizeof(porta));
+	is.read((char*)&id,sizeof(id));
+	return is;
+}
+
+std::ostream& Noh::write(std::ostream& os)
+{
+	os.write((char*)&ip,sizeof(ip));
+	os.write((char*)&porta,sizeof(porta));
+	os.write((char*)&id,sizeof(id));
+	return os;
+}
+
+//------------------------------------------------------------------------------
+//      Cliente
+//------------------------------------------------------------------------------
+
+Cliente::Cliente()
+{
+    ping=versao=opcoes=0;
+    mtu=mru=2000;
+}
+
+int Cliente::read(unsigned char*& pkt)
+{
+    versao=*((unsigned long*)pkt);
+    pkt+=sizeof(versao);
+    opcoes=*((unsigned long*)pkt);
+    pkt+=sizeof(opcoes);
+    Noh::read(pkt);
+    mtu=*((unsigned short*)pkt);
+    pkt+=sizeof(mtu);
+    mru=*((unsigned short*)pkt);
+    pkt+=sizeof(mru);
+    return 0;
+}
+
+int Cliente::write(unsigned char*& pkt) const
+{
+    *((unsigned long*)pkt)=versao;
+    pkt+=sizeof(versao);
+    *((unsigned long*)pkt)=opcoes;
+    pkt+=sizeof(opcoes);
+    Noh::write(pkt);
+    *((unsigned short*)pkt)=mtu;
+    pkt+=sizeof(mtu);
+    *((unsigned short*)pkt)=mru;
+    pkt+=sizeof(mru);
+    return 0;
+}
+
+istream& Cliente::read(istream& is)
+{
+	is.read((char*)&versao,sizeof(versao));
+	is.read((char*)&opcoes,sizeof(opcoes));
+	Noh::read(is);
+	return is;
+}
+
+ostream& Cliente::write(ostream& os)
+{
+	os.write((char*)&versao,sizeof(versao));
+	os.write((char*)&opcoes,sizeof(opcoes));
+	Noh::write(os);
+	return os;
+}
+
+//------------------------------------------------------------------------------
+//      Usuario
+//------------------------------------------------------------------------------
+
+Usuario::Usuario()
+{
+	nick[0]=0;
+    rx=tx=0;
+}
+
+Usuario::Usuario(const Usuario& base)
+{
+    memcpy(nick,base.nick,TAMNICK);
+    memcpy(h.b,base.h.b,16);
+    rx=base.rx;
+    tx=base.tx;
+    codCli=base.codCli;
+}
+
+int Usuario::mudaNick(std::string novo)
+{
+	memset(nick,0,TAMNICK);
+	novo.copy(nick,TAMNICK-1);
+	return 0;
+}
+
+int Usuario::read(unsigned char*& pkt)
+{
+    Hash128::read(pkt);
+    memcpy(nick,pkt,TAMNICK);
+    pkt+=TAMNICK;
+    return 0;
+}
+
+int Usuario::write(unsigned char *&pkt) const
+{
+    Hash128::write(pkt);
+    memcpy(pkt,nick,TAMNICK);
+    pkt+=TAMNICK;
+    return 0;
+}
+
+istream& Usuario::read(istream& is)
+{
+	is.read((char*)h.b,sizeof(h));
+	is.read(nick,TAMNICK);
+	is.read((char*)&rx,sizeof(rx));
+	is.read((char*)&tx,sizeof(tx));
+	return is;
+}
+
+ostream& Usuario::write(ostream& os)
+{
+	os.write((char*)h.b,sizeof(h));
+	os.write(nick,TAMNICK);
+	os.write((char*)&rx,sizeof(rx));
+	os.write((char*)&tx,sizeof(tx));
+	return os;
+}
+
+//------------------------------------------------------------------------------
+//      Slot
+//------------------------------------------------------------------------------
+
+Slot::Slot()
+{
+    temp.mudaTamanho(TAMBUFFER);
+    c=NULL;
+    recebendo=NULL;
+    reset();
+}
+
+Slot::~Slot()
+{
+	reset();
+}
+
+int Slot::reset()
+{
+	estado=0;
+	estadoRX=NOVO;
+	if(c!=NULL)
+	{
+        delete c;
+		c=NULL;
+	}
+    if(recebendo!=NULL)
+    {
+        delete recebendo;
+        recebendo=NULL;
+    }
+    return 0;
+}
+
+int Slot::desconectar()
+{
+    if(pegaEstado()!=0)
+        c->desconectar();
+	reset();
+    return 0;
+}
+
+int Slot::pegaEstado()
+{
+    m.trava();
+    {
+        if(c!=NULL)
+        {
+            if(!c->ativa())
+                reset();
+        }
+        else
+            reset();
+    }
+    m.destrava();
+    return estado;
+}
+
+int Slot::setaEstado(int novo)
+{
+    m.trava();
+	estado=novo;
+    m.destrava();
+    return 0;
+}
+
+int Slot::enviar(Buffer *pkt)
+{
+	m.trava();
+    int retorno=-1;
+    if(pegaEstado()!=0)
+    {
+        CabecalhoLVL0 *cab=(CabecalhoLVL0*)pkt->dados;
+        int enviados=c->enviar((char*)pkt->dados,cab->tam);
+        if(enviados==cab->tam)
+            retorno=0;
+    }
+    delete pkt;
+    m.destrava();
+    return retorno;
+}
+
+Buffer* Slot::receber()
+{
+	int qtd;
+	m.trava();
+	do
+	{
+		temp.reset();
+		qtd=c->receber((char*)temp.pntE, temp.livres());
+		if(qtd>0)
+			temp.pntE+=qtd;
+		while(temp.disponiveis()>0)
+		{
+			if(estadoRX==NOVO)					//esperando novo pacote
+			{
+				recebendo=new Buffer(2);
+				if(recebendo->append(temp,2)==1)
+					estadoRX=ESPERA_TAMANHO;
+				else
+					estadoRX=INICIO_DADOS;
+			}
+			else
+			{
+			    if(recebendo==NULL)
+                    estadoRX=NOVO;
+				if(estadoRX==ESPERA_TAMANHO)	//esperando segundo byte de tamanho
+				{
+					if(recebendo->append(temp,1)==1)
+						estadoRX=INICIO_DADOS;
+				}
+				else if(estadoRX==INICIO_DADOS)	//tamanho recebido
+				{
+					CabecalhoLVL0 *cab=(CabecalhoLVL0*)recebendo->dados;
+					recebendo->mudaTamanho(cab->tam);
+					estadoRX=DADOS;
+				}
+				else if(estadoRX==DADOS)		//receber comando e dados
+				{
+					recebendo->append(temp,recebendo->livres());
+					if(recebendo->livres()==0)	//aceitacao
+					{
+//						logar("pacote completo recebido");
+						recebidos.push(recebendo);
+						recebendo=NULL;
+						estadoRX=NOVO;
+					}
+				}
+			}
+		}
+	} while(qtd>0);
+
+	Buffer *retorno=NULL;
+	if(!recebidos.empty())
+	{
+		retorno=recebidos.front();
+		recebidos.pop();
+	}
+	m.destrava();
+	return retorno;
+}
+
+//------------------------------------------------------------------------------
+//      ArraySlots
+//------------------------------------------------------------------------------
+
+ArraySlots::ArraySlots(int num)
+{
+	slots=NULL;
+	numSlots=0;
+	mudaNumSlots(num);
+}
+
+ArraySlots::~ArraySlots()
+{
+    if((slots!=NULL)&&(numSlots>0))
+        delete[] slots;
+}
+
+int ArraySlots::mudaNumSlots(int num)
+{
+	if(num>0)
+	{
+		Slot *temp=new Slot[num];
+		if(temp==NULL)
+			return -1;		//erro de alocacao
+		if((slots!=NULL)&&(numSlots>0))
+			delete[] slots;
+		slots=temp;
+	}
+	else if((slots!=NULL)&&(numSlots>0))
+	{
+		delete[] slots;
+		slots=NULL;
+	}
+	numSlots=num;
+	return 0;
+}
+
+Slot* ArraySlots::at(int num) const
+{
+    if((num>=0)&(num<numSlots))
+        return &slots[num];
+    return NULL;
+}
+
+Slot* ArraySlots::operator[](const Hash128& user) const
+{
+    for(int x=0;x<numSlots;x++)
+    	if(slots[x].iU.cmp(user)==0)
+			return &slots[x];
+    return NULL;
+}
+
+int ArraySlots::aloca()
+// busca e aloca (estado 0->1) slot livre
+// retorna num do slot ou -1 caso todos estejam ocupados
+{
+    m.trava();
+    {
+        for(int x=0;x<numSlots;x++)
+        {
+            if(slots[x].pegaEstado()==0)
+            {
+                if(slots[x].setaEstado(1)==0)
+                {
+                    m.destrava();
+                    return x;
+                }
+            }
+        }
+    }
+    m.destrava();
+    return -1;
+}
+
+int ArraySlots::conectar(const char *ip, const unsigned short porta)
+{
+    Slot *s;
+    int ns=aloca();
+    if(ns<0)
+        return ns;  //nenhum slot disponivel
+    s=&slots[ns];
+    if(s->c==NULL)
+        s->c=new Conexao();
+    Conexao *c=s->c;
+    c->pai=this;
+    c->id=ns;
+    c->registraCallback(ClienteP2P::tratar);
+    return c->conectar(ip,porta);
+}
+
+int ArraySlots::conectar(std::string ip, const unsigned short porta)
+{
+	return conectar(ip.c_str(),porta);
+}
+
+int ArraySlots::desconectar(int nslot)
+{
+	if(nslot<0)
+		for(int x=0;x<numSlots;x++)
+			slots[x].desconectar();
+	else if(nslot<numSlots)
+		slots[nslot].desconectar();
+	else
+		return -1;
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+//      ListaHash128
+//------------------------------------------------------------------------------
+
+int ListaHash128::insere(const Hash128& h)
+{
+	m.trava();
+	if(binary_search(lista.begin(),lista.end(),h))
+	{
+		m.destrava();
+		return -1;	//jah existe
+	}
+	list<Hash128>::iterator i=upper_bound(lista.begin(),lista.end(),h);
+	lista.insert(i,h);
+	m.destrava();
+	return 0;
+}
+
+void ListaHash128::remove(const Hash128& h)
+{
+	m.trava();
+	lista.remove(h);
+	m.destrava();
+}
+
+bool ListaHash128::busca(const Hash128& h)
+{
+	m.trava();
+	bool retorno=binary_search(lista.begin(),lista.end(),h);
+	m.destrava();
+	return retorno;
+}
+
+int ListaHash128::tamanho()
+{
+	m.trava();
+	int tam=lista.size();
+	m.destrava();
+	return tam;
+}
+
+void ListaHash128::limpa()
+{
+	m.trava();
+	lista.clear();
+	m.destrava();
+}
+
+istream& ListaHash128::read(istream& is)
+{
+	Hash128 tmp;
+	unsigned short numero;
+
+	if(is.read((char*)&numero,sizeof(numero)))
+		for(int n=0;n<numero;n++)
+			if(is.read((char*)tmp.h.b,sizeof(tmp.h)))
+				insere(tmp);
+			else
+				break;
+	return is;
+}
+
+ostream& ListaHash128::write(ostream& os)
+{
+	m.trava();
+	unsigned short numero=lista.size();
+	if(os.write((char*)&numero,sizeof(numero)))
+	{
+		list<Hash128>::iterator i=lista.begin();
+		while(i!=lista.end())
+		{
+			os.write((char*)(*i).h.b,sizeof((*i).h));
+			i++;
+		}
+	}
+	m.destrava();
+	return os;
+}
+
+//------------------------------------------------------------------------------
+//      ListaUsuarios
+//------------------------------------------------------------------------------
+
+bool ListaUsuarios::insere(const Hash128& h, Usuario* u)
+{
+	bool ret=true;
+	m.trava();
+	if(lista.find(h)!=lista.end())
+		ret=false;	//jah existe
+	else
+		lista[h]=u;
+	m.destrava();
+	return ret;
+}
+
+void ListaUsuarios::remove(const Hash128& h)
+{
+	m.trava();
+	lista.erase(h);
+	m.destrava();
+}
+
+Usuario* ListaUsuarios::busca(const Hash128& h)
+{
+	Usuario* ret=NULL;
+	m.trava();
+	map<Hash128,Usuario*>::iterator i=lista.find(h);
+	if(i!=lista.end())
+		ret=i->second;
+	m.destrava();
+	return ret;
+}
+
+int ListaUsuarios::tamanho()
+{
+	m.trava();
+	int tam=lista.size();
+	m.destrava();
+	return tam;
+}
+
+void ListaUsuarios::limpa()
+{
+	m.trava();
+	lista.clear();
+	m.destrava();
+}
+
+istream& ListaUsuarios::read(istream& is)
+{
+    Usuario tmp;
+    while(tmp.read(is))
+        lista[tmp]=new Usuario(tmp);
+	return is;
+}
+
+ostream& ListaUsuarios::write(ostream& os)
+{
+    map<Hash128,Usuario*>::iterator i=lista.begin();
+    while(i!=lista.end())
+    {
+        i->second->write(os);
+        i++;
+    }
+	return os;
+}
+
+//------------------------------------------------------------------------------
+//      Cliente
+//------------------------------------------------------------------------------
+
+ClienteP2P::ClienteP2P()
+{
+    strcpy(iU.nick,"Noob");
+	iU.Hash128::random();
+    serverSock.pai=this;
+    serverSock.registraCallback(tratarServer);
+}
+
+ClienteP2P::~ClienteP2P()
+{
+}
+
+void ClienteP2P::registraUI(ClienteP2PUI *ui)
+{
+	p2pui=ui;
+}
+
+int ClienteP2P::abrir(std::string dir)
+{
+	ifstream arq;
+	IOArquivo ret;
+	string arquivo;
+
+	arquivo="id.dat";
+	ret=abrirID(arquivo);
+	if(ERRO_VERSAO==ret)
+		logar("Versao incorreta do arquivo "+arquivo+"\n");
+	else if(ERRO_IO==ret)
+		logar("Erro ao ler o arquivo "+arquivo+"\n");
+
+//TODO:	iC.read(arq);
+
+	arquivo="usuarios.dat";
+	arq.open(arquivo.c_str(),ios::binary|ios::in);
+	if(arq.good())
+		usuarios.read(arq);
+	else
+		logar("Erro ao ler o arquivo "+arquivo+"\n");
+    arq.close();
+    arq.clear();
+
+	return 0;
+}
+
+int ClienteP2P::salvar(std::string dir)
+{
+	ofstream arq;
+	IOArquivo ret;
+	string arquivo;
+
+	arquivo="id.dat";
+	ret=salvarID(arquivo);
+	if(ERRO_IO==ret)
+		logar("Erro ao gravar o arquivo "+arquivo+"\n");
+
+//TODO:	iC.write(arq);
+
+	arquivo="usuarios.dat";
+	arq.open(arquivo.c_str(),ios::binary|ios::out|ios::trunc);
+	if(arq.good())
+		usuarios.write(arq);
+	else
+		logar("Erro ao gravar o arquivo "+arquivo+"\n");
+    arq.close();
+    arq.clear();
+
+	return 0;
+}
+
+IOArquivo ClienteP2P::abrirID(std::string arquivo)
+{
+	ifstream arq;
+
+	arq.open(arquivo.c_str(),ios::binary|ios::in);
+	if(arq.good())
+	{
+		int ver;
+		if(arq.read((char*)&ver,sizeof(ver)))
+		{
+			if(VERSAOINFO==ver)
+			{
+				if(iU.read(arq))
+				{
+					amigos.limpa();
+					if(amigos.read(arq))
+						return OK;
+				}
+			}
+			else
+				return ERRO_VERSAO;
+		}
+	}
+	return ERRO_IO;
+}
+
+IOArquivo ClienteP2P::salvarID(std::string arquivo)
+{
+	ofstream arq;
+
+	arq.open(arquivo.c_str(),ios::binary|ios::out|ios::trunc);
+	if(arq.good())
+	{
+		int v=VERSAOINFO;
+		if(arq.write((char*)&v,sizeof(v)))
+			if(iU.write(arq))
+				if(amigos.write(arq))
+					return OK;
+	}
+	return ERRO_IO;
+}
+
+int ClienteP2P::esperarConexoes(unsigned short porta)
+{
+	int retorno=0;
+	serverSock.desconectar();
+	if(0==porta)
+		porta=iC.porta;
+	if((retorno=serverSock.ouvir(porta))==0)
+	{
+		logar("Esperando conexoes na porta "+int2str(porta));
+		iC.porta=porta;
+	}
+	else
+		logar("Erro ao tentar abrir porta "+int2str(porta));
+	return retorno;
+}
+
+int ClienteP2P::enviarMsg(string msg, const Hash128* user)
+{
+	return enviarMsg(msg.c_str(),user);
+}
+
+int ClienteP2P::enviarMsg(const char *msg, const Hash128* user)
+//  -1 = desconectado
+//  -2 = erro
+{
+    Slot *s=slots[*user];
+    if(s==NULL)
+    {
+		s=slots[0];
+		if(s==NULL)
+			return -1;
+    }
+
+    unsigned short tam=strlen(msg);
+    Buffer *p=new Buffer(tam+sizeof(CabecalhoLVL0));
+    if(p==NULL)
+        return -2;
+
+    CabecalhoLVL0 *tmp=(CabecalhoLVL0*)p->dados;
+    tmp->tam=tam+sizeof(CabecalhoLVL0);
+    tmp->com=MENSAGEM;
+    memcpy((p->dados+sizeof(CabecalhoLVL0)),msg,tam);
+    if(s->enviar(p))
+        return -2;
+
+    return 0;
+}
+
+void ClienteP2P::ping()
+{
+	for(int x=0;x<slots.pegaNumSlots();x++)
+		enviaPing(slots[x]);
+}
+
+int ClienteP2P::enviaLogin(Slot *slot)
+{
+	Buffer *login=new Buffer(58);
+	CabecalhoLVL0 *cab=(CabecalhoLVL0*)login->dados;
+	cab->tam=58;
+	cab->com=LOGIN;
+	unsigned char *dados=login->dados+sizeof(CabecalhoLVL0);
+	iC.write(dados);
+	iU.write(dados);
+	if(slot->enviar(login))
+		return -1;
+	return 0;
+}
+
+int ClienteP2P::enviaPing(Slot *slot)
+{
+	Buffer *ping=new Buffer(8);
+	CabecalhoLVL0 *cab=(CabecalhoLVL0*)ping->dados;
+	cab->tam=8;
+	cab->com=PING;
+	unsigned char *dados=ping->dados+sizeof(CabecalhoLVL0);
+	unsigned long timestamp=(clock()/(CLOCKS_PER_SEC/1000));
+	*((unsigned long*)dados)=timestamp;
+	if(slot->enviar(ping))
+		return -1;
+	return 0;
+}
+
+int ClienteP2P::enviaPong(unsigned long timestamp, Slot *slot)
+{
+	Buffer *pong=new Buffer(8);
+	CabecalhoLVL0 *cab=(CabecalhoLVL0*)pong->dados;
+	cab->tam=8;
+	cab->com=PONG;
+	unsigned char *dados=pong->dados+sizeof(CabecalhoLVL0);
+	*((unsigned long*)dados)=timestamp;
+	if(slot->enviar(pong))
+		return -1;
+	return 0;
+}
+
+int ClienteP2P::tratar(Buffer *pacote, Slot *slot)
+{
+    CabecalhoLVL0 *cab=(CabecalhoLVL0*)pacote->dados;
+    unsigned char *dados=pacote->dados+sizeof(CabecalhoLVL0);
+
+    if(cab->com==LOGIN)
+    {
+		#ifdef LOGAR_COMANDOS
+			logar("CMD_LOGIN");
+		#endif
+        slot->iC.read(dados);
+        slot->iU.read(dados);
+        slot->setaEstado(2);
+        Usuario *tmp=usuarios.busca(slot->iU);
+        if(tmp==NULL)
+        	usuarios.insere(slot->iU,new Usuario(slot->iU));
+    }
+    else if(cab->com==MENSAGEM)
+    {
+		#ifdef LOGAR_COMANDOS
+			logar("CMD_MENSAGEMDIRETA");
+		#endif
+    	string stmp=string((char*)dados,(size_t)(cab->tam-sizeof(CabecalhoLVL0)));
+        mostrarMsg(&slot->iU,stmp);
+    }
+    else if(cab->com==PING)
+    {
+    	#ifdef LOGAR_COMANDOS
+			logar("CMD_PING");
+		#endif
+    	unsigned long timestamp=*((unsigned long*)dados);
+    	enviaPong(timestamp, slot);
+    }
+	else if(cab->com==PONG)
+    {
+    	#ifdef LOGAR_COMANDOS
+			logar("CMD_PONG");
+		#endif
+    	unsigned long base=*((unsigned long*)dados);
+    	unsigned long agora=(clock()/(CLOCKS_PER_SEC/1000));
+    	slot->iC.ping=agora-base;
+    }
+    delete pacote;
+    return 0;
+}
+
+int ClienteP2P::tratar(Conexao *con, long codeve, long coderro[])
+{
+	if(con==NULL)
+		return -1;
+	ClienteP2P *pai=(ClienteP2P*)con->pai;
+	if(pai==NULL)
+		return -1;
+
+	Slot *s=pai->slots.at(con->id);
+	if(s==NULL)
+		return -1;
+
+	s->iC.ip=con->pegaInfo()->sin_addr.s_addr;
+	s->iC.porta=con->pegaInfo()->sin_port;
+
+	if(codeve & FD_CONNECT)
+	{
+		#ifdef LOGAR_SOCKET
+			logar("FD_CONNECT");
+		#endif
+		if(coderro[FD_CONNECT_BIT]!=0)
+			return 1;
+	}
+	if(codeve & FD_READ)
+	{
+		#ifdef LOGAR_SOCKET
+			logar("FD_READ");
+		#endif
+		if(coderro[FD_READ_BIT]!=0)
+			return 1;
+		else
+		{
+			Buffer *p;
+			while((p=s->receber())!=NULL)
+				pai->tratar(p,s);
+		}
+	}
+	if(codeve & FD_WRITE)
+	{
+		#ifdef LOGAR_SOCKET
+			logar("FD_WRITE");
+		#endif
+		if(coderro[FD_WRITE_BIT]!=0)
+			return 1;
+		else
+			pai->enviaLogin(s);
+	}
+	if(codeve & FD_CLOSE)
+	{
+		#ifdef LOGAR_SOCKET
+			logar("FD_CLOSE");
+		#endif
+		return 1;	//matar thread...
+	}
+	return 0;
+}
+
+int ClienteP2P::tratarServer(Conexao *con, long codeve, long coderro[])
+{
+    ClienteP2P *pai;
+    int ns;
+    Slot *s;
+
+    if((pai=(ClienteP2P*)con->pai)==NULL)
+        return -1;
+
+	if(codeve & FD_ACCEPT)
+	{
+		#ifdef LOGAR_SOCKET
+			logar("FD_ACCEPT");
+		#endif
+        ns=pai->slots.aloca();
+        if(ns<0)
+        {
+            con->recusar();
+            logar("Conexão recusada");
+            return 0;
+        }
+        s=pai->slots[ns];
+		s->reset();
+        s->c=con->aceitar();
+        if(s->c!=NULL)
+        {
+            s->c->pai=con->pai;
+            s->c->id=ns;
+            s->c->registraCallback(tratar);
+        }
+	}
+	if(codeve & FD_CLOSE)
+		return 1;	//matar thread...
+	return 0;
+}
---
+++ ProtocoloP2P.h
@@ -0,0 +1,347 @@
+#ifndef MBSUTIL_PROTOCOLO_H
+#define MBSUTIL_PROTOCOLO_H
+
+#include "MBSUtil.h"
+#include <cstdio>
+#include <string>
+#include <map>
+#include <list>
+#include <queue>
+#include <iostream>
+
+/*	Descricao do protocolo P2P
+
+	Formato geral dos pacotes:
+    [cabecalho][dados]
+
+    Cabecalho (info geral do pacote):
+    [tamanho][comando]
+    tamanho = unsigned short = tamanho total do pacote
+    comando = unsigned short = enum Protocolo::Comandos
+
+    Dados (depende do comando):
+    sizeof(dados)=tamanho-sizeof(cabecalho)
+*/
+namespace Protocolo
+{
+	enum Comandos
+	{
+		LOGIN,
+		/*	pacote com informacoes sobre o cliente e usuario
+			[infoCliente][Usuario]
+				infoCliente = [versao][opcoes][Noh][mtu][mru]
+					Noh = [ip][porta][id]
+					sizeof(Noh)=10
+					sizeof(infoCliente)=12+sizeof(Noh)=22
+				infoUsuario = [Hash128][Nick]
+					sizeof(infoUsuario)=16+TAMNICK=32
+			sizeof(dados)=54
+		*/
+		MENSAGEM,
+		/*	Mensagem instantanea (texto/chat)
+			[char[] (nao-ASCIIZ)]
+		*/
+		PING,
+		/*	Ping
+			[unsigned long timestamp]
+		*/
+		PONG,
+		/*	Resposta ao ping
+			[unsigned long timestamp] (copia do recebido em ping)
+		*/
+		ROTEAR,
+		/*	Pedido de roteamento do pacote pra cliente com id baixa
+			[remetente][destinatario][dados]
+			[Noh][Hash128]
+		*/
+		RETORNO,
+		/*	Pedido de conexao de callback
+		*/
+		BUSCAR,
+		/*	Broadcast de busca
+			[unsigned char TTL][criterio]
+		*/
+		ACK,
+		/*	Acknowledge (pra chat p.e)
+		*/
+		USER,
+		/*	protocolo definido pelo usuario
+			[unsigned char codigo do protocolo][dados]
+		*/
+
+	};
+	//	Constantes diversas
+	const int PORTA_PADRAO=6661;
+	const int TAMNICK=15+1;
+	const int VERSAOINFO=1;		//versao dos arquivos
+	typedef enum
+	{	//Maquina de estados para RX de Slot
+		NOVO,			//esperando novo pacote
+		ESPERA_TAMANHO,	//esperando segundo byte de tamanho
+		INICIO_DADOS,	//alocando espaco para dados
+		DADOS,			//recebendo dados
+	} EstadosRX;
+	typedef enum
+	{
+		ERRO_IO,		//erro de leitura/escrita
+		ERRO_VERSAO,	//versao incorreta do arquivo
+		OK,				//tudo ok
+	} IOArquivo;
+};
+
+using std::istream;
+using std::ostream;
+
+/*	Classe Noh
+	Responsavel por armazenar informacoes sobre um endereco de rede TCP/IP.
+*/
+class Noh
+{
+public:
+//TODO:	unsigned long codigo;	//associacao com Usuario
+	unsigned long ip;
+	unsigned short porta;
+	unsigned long id;
+
+	Noh();
+	bool idAlta() { return ((ip!=0)?(ip==id):false); }
+	int read(unsigned char *&pkt);
+	int write(unsigned char *&pkt) const;
+    istream& read(istream& is);
+    ostream& write(ostream& os);
+};
+
+/*	Classe Cliente
+	Responsavel por descrever um cliente da rede P2P, protocolos aceitos etc.
+*/
+class Cliente : public Noh
+{
+public:
+	unsigned long versao;
+	unsigned long opcoes;
+	unsigned short mtu;
+	unsigned short mru;
+	unsigned long ping;
+
+	Cliente();
+	int read(unsigned char*& pkt);
+	int write(unsigned char*& pkt) const;
+	istream& read(istream& is);
+    ostream& write(ostream& os);
+};
+
+/*	Classe Usuario
+	Descreve um usuario da rede
+*/
+class Usuario : public Hash128
+{
+public:
+	char nick[Protocolo::TAMNICK];	//TODO: nick deveria ser string...
+	unsigned long rx;				//volume de dados recebidos
+	unsigned long tx;				//volume de dados enviados
+
+	int classificacao() const
+	{
+		return rx-tx;
+		//amigo=k
+		//classificacao=saldo+amigo
+	}
+
+	unsigned long codMsg;			//id sequencial das mensagens (ACK)
+	unsigned long codCli;			//codigo do noh (ou cliente) na lista de Nohs
+
+	Usuario();
+	Usuario(const Usuario& base);
+	int mudaNick(std::string novo);
+
+	int read(unsigned char*& pkt);
+	int write(unsigned char*& pkt) const;
+    istream& read(istream& is);
+    ostream& write(ostream& os);
+};
+
+/*	Classe Slot
+	Cuida de toda parte de baixo nivel da comunicacao, envio/recepcao de pacotes
+*/
+class Slot
+{
+public:
+	friend class ClienteP2P;
+	friend class ArraySlots;
+
+	Cliente iC;
+	Usuario iU;
+
+	Slot();
+	~Slot();
+	int desconectar();
+	int pegaEstado();
+	int setaEstado(int estado);
+	int enviar(Buffer *pkt);
+	Buffer* receber();
+protected:
+	Mutex m;
+	Conexao *c;
+
+	//Maquina de estados e fila de RX
+	Buffer temp;					//usado na recepcao de pacotes
+	Buffer *recebendo;				//pacote sendo recebido
+	Protocolo::EstadosRX estadoRX;	//estados de recepcao
+	std::queue<Buffer*> recebidos;	//fila de pacotes prontos
+
+	int estado;
+	int reset();
+};
+
+/*	Classe ArraySlots
+	Gerencia recursos de rede, limita o max de conexoes simultaneas, aloca e
+	libera Slots
+*/
+class ArraySlots
+{
+public:
+	ArraySlots(int num=10);
+	~ArraySlots();
+	int pegaNumSlots() const { return numSlots; }
+	int mudaNumSlots(int num);
+	Slot* at(int num) const;
+	Slot* operator[](int i) const { return &slots[i]; }
+	Slot* operator[](const Hash128& user) const;
+	int aloca();
+	//TODO:	int libera();	//mata slot com a classificacao mais baixa e aloca
+	int conectar(const char *ip, const unsigned short porta);
+	int conectar(std::string ip, const unsigned short porta);
+	int desconectar(int nslot=-1);	//-1 = todos
+protected:
+	Mutex m;
+	int numSlots;
+	Slot *slots;
+};
+
+/*	Classe ListaHash128
+	Fornece acesso facil e sincronizado para listas de hashs
+	IMPORTANTE: todos acessos devem ser protegidos pelo mutex
+*/
+class ListaHash128
+{
+public:
+	Mutex m;
+	std::list<Hash128> lista;
+
+	ListaHash128() {}
+	~ListaHash128() { limpa(); }
+
+	int insere(const Hash128& h);
+	void remove(const Hash128& h);
+	bool busca(const Hash128& h);
+	int tamanho();
+	void limpa();
+    istream& read(istream& is);
+    ostream& write(ostream& os);
+};
+
+/*	Classe ListaUsuarios
+	Fornece acesso facil e sincronizado para listas de usuarios
+*/
+class ListaUsuarios
+{
+public:
+	Mutex m;
+	std::map<Hash128,Usuario*> lista;
+
+	ListaUsuarios() {}
+	~ListaUsuarios() { limpa(); }
+
+	bool insere(const Hash128& h, Usuario* u);
+	void remove(const Hash128& h);
+	Usuario* busca(const Hash128& h);
+	int tamanho();
+	void limpa();
+    istream& read(istream& is);
+    ostream& write(ostream& os);
+};
+
+class ListaNohs
+{
+public:
+	Mutex m;
+	std::list<Noh*> lista;
+
+	void insere(const Noh& no)
+		{}
+	void remove(const Noh& no)
+		{}
+	Noh* busca(const Noh& no);
+	int tamanho()
+		{ return lista.size(); }
+	void limpa()
+		{ lista.clear(); }
+    istream& read(istream& is);
+    ostream& write(ostream& os);
+};
+
+class EventoP2PUI
+{
+public:
+	const enum tipoEvento
+	{
+		RELATORIO,		//[mensagem] deve ser colocada na area de log
+		MSG_CHAT_RX,	//[usuario] enviou [mensagem] de chat
+		MSG_CHAT_TX		//envio da mensagem [id] concluido
+	} tipo;
+	unsigned long id;
+	bool erro;			//indica erro
+	Hash128 usuario;
+	std::string mensagem;
+
+	EventoP2PUI(std::string& msg)
+		: tipo(RELATORIO), erro(false), mensagem(msg) {}
+};
+
+/*	Classe ClienteP2PUI
+	Interface do responsavel pelo tratamento de eventos do cliente p2p (callback).
+	Deve ser herdada e reimplementada.
+*/
+class ClienteP2PUI
+{
+public:
+	virtual void relatorio(const std::string& mensagem)
+		{ std::cerr << mensagem << std::endl; }
+	virtual void mostraMensagemChat(const Hash128* remetente, const std::string& mensagem)
+		{ std::cerr << remetente->toString() << ":" << mensagem << std::endl; }
+//	virtual void trataEvento(EventoP2PUI* evento);
+};
+
+class ClienteP2P
+{
+public:
+	ArraySlots slots;
+	Cliente iC;
+	Usuario iU;
+
+	ListaUsuarios usuarios;		//arvore com todos usuarios conhecidos
+	ListaHash128 amigos;		//lista de amigos
+	ListaHash128 blacklist;		//lista negra
+
+	ClienteP2P();
+	~ClienteP2P();
+	void registraUI(ClienteP2PUI *ui);	//registra callbacks de UI
+	int abrir(std::string dir=".");		//abre todas as listas e info
+	int salvar(std::string dir=".");	//salva todas as listas e info
+	Protocolo::IOArquivo abrirID(std::string arquivo);
+	Protocolo::IOArquivo salvarID(std::string arquivo);
+	int esperarConexoes(unsigned short porta=0);	//0=iC.porta
+	int enviarMsg(std::string msg, const Hash128* user);
+	int enviarMsg(const char *msg, const Hash128* user);
+	void ping();						//manda pedido de ping p/ todos slots
+protected:
+	int enviaLogin(Slot *slot);
+	int enviaPing(Slot *slot);
+	int enviaPong(unsigned long timestamp, Slot *slot);
+	int tratar(Buffer *pacote, Slot *slot);		//trata pacote recebido
+private:
+	Conexao serverSock;
+	static int tratar(Conexao *con, long codeve, long coderro[]);
+	static int tratarServer(Conexao *con, long codeve, long coderro[]);
+};
+#endif
