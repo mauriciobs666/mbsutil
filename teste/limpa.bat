09b71bae560ba03f3c07599dea569e66df0dc193
mauriciobs <mauriciobs@6ba988c5-0224-0410-acf2-7d9395eead91>
2006-12-08 16:56:17 +0000

    Atualizado arquivo de projeto.
    git-svn-id: https://mbsutil.googlecode.com/svn@67 6ba988c5-0224-0410-acf2-7d9395eead91

--- teste/limpa.bat
+++ teste/limpa.bat
@@ -1,17 +1,7 @@
 @echo off
-del MBSUtil_private.h
-del MBSUtil_private.rc
-del Makefile.win
-del teste.layout
-del teste.depend
 del c:\temp\*.o
-del c:\temp\*.obj
-del *.tds
-del *.~df
-del *.~h
-del *.~cp
-del *.~bp
-del *.max
-del *.rmx
-del *.res
+del *.layout
+del *.depend
+strip *.exe
+upx -9 *.exe
 pause
