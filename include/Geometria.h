a9e56028d2e6da4b9cf253c2e6c97e63c200480f
mauriciobs <mauriciobs@6ba988c5-0224-0410-acf2-7d9395eead91>
2006-12-12 20:10:20 +0000

    Limpeza.
    git-svn-id: https://mbsutil.googlecode.com/svn@69 6ba988c5-0224-0410-acf2-7d9395eead91

--- include/Geometria.h
+++ include/Geometria.h
@@ -1,18 +1,20 @@
 /*
-  Name: Vetores
-  Copyright: Todos direitos reservados - 2005
-  Author: Mauricio Bieze Stefani
-  Date: 27/09/05 20:51
-  Description: 
+	Copyright 2006 - Mauricio Bieze Stefani
 */
 
 #ifndef geometriaH
 #define geometriaH
-
 #include <ostream>
 
 typedef float Real;
 const Real PI=3.14159265;
+
+class Vetor2i
+{
+public:
+	int x,y;
+	Vetor2i(int xx=0, int yy=0) : x(xx), y(yy) {}
+};
 
 class Vetor2D
 {
@@ -37,20 +39,20 @@ public:
     Vetor3D& operator-=(const Vetor3D& v);
     Vetor3D& operator*=(Real r);			//escala
 	Vetor3D& operator*=(const Vetor3D& v);	//produto vetorial
-	
+
 	bool operator==(const Vetor3D& v) const { return ((x==v.x)&(y==v.y)&(z==v.z)); }
 	bool operator!=(const Vetor3D& v) const { return ((x!=v.x)|(y!=v.y)|(z!=v.z)); }
 
 	Real norma() const;
 	Vetor3D unit() const;
 	Vetor3D& normalizar();
-    
+
     Real dist(const Vetor3D& v) const { return (*this-v).norma(); }
     Real dot(const Vetor3D& v) const { return (x*v.x + y*v.y + z*v.z); }
 
     Vetor3D gira(const Vetor3D& ang) const;			//angulos em graus
     Vetor3D gira(Real ax, Real ay, Real az) const;	//angulos em graus
-    
+
 	Real angulo(const Vetor3D& v) const;
 	Real cosseno(const Vetor3D& v) const;
 };
