e5e2309026962041ded82b01eeb0a99fdfc81cfd
mauriciobs <mauriciobs@6ba988c5-0224-0410-acf2-7d9395eead91>
2006-12-18 16:19:30 +0000

    Movi glImagem pra cah.
    git-svn-id: https://mbsutil.googlecode.com/svn@71 6ba988c5-0224-0410-acf2-7d9395eead91

---
+++ glImagem.cpp
@@ -0,0 +1,123 @@
+/*
+	MBS-Util
+	Copyright (C) 2006 - Mauricio Bieze Stefani
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "glimagem.h"
+
+using namespace std;
+
+#undef DEBUG
+
+glImagem::glImagem()
+{
+	glId=0;
+	pixels=NULL;
+}
+
+glImagem::~glImagem()
+{
+	if(pixels)
+		delete[] pixels;
+}
+
+int glImagem::desenha() const
+{
+	if(pixels)
+	{
+		glDrawPixels(w,h,format,GL_UNSIGNED_BYTE,pixels);
+		return 0;
+	}
+	return -1;
+}
+
+//	Carrega textura na placa de video
+int glImagem::bind()
+{
+	if(NULL==pixels)
+		return -1;
+
+	if(glId==0)
+	{
+		glGenTextures(1,&glId);
+		if(glId==0)
+		{
+			#ifdef DEBUG
+				printf("Erro ao alocar nome pra textura\n");
+			#endif
+			return -1;
+		}
+		glBindTexture(GL_TEXTURE_2D, glId);
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+   		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+   		glTexImage2D(GL_TEXTURE_2D, 0, format, w, h, 0, format, GL_UNSIGNED_BYTE, pixels);
+   		#ifdef DEBUG
+			printf("Textura carregada\n");
+		#endif
+	}
+	else
+	{
+		glBindTexture(GL_TEXTURE_2D, glId);
+		#ifdef NEVER
+			printf("Textura ativada\n");
+		#endif
+	}
+	return 0;
+}
+
+int glImagem::resize()
+{
+	if(NULL==pixels)
+		return -1;
+
+	int novoX=4;
+	int novoY=4;
+	while(novoX<w) novoX*=2;	//procura menor potencia de 2
+	while(novoY<h) novoY*=2;
+
+	int novoPitch=novoX;
+	novoPitch*=(format==GL_RGB)?3:4;
+
+	ratio.x=(float)w/(float)novoX;
+	ratio.y=(float)h/(float)novoY;
+
+	GLubyte *p=new GLubyte[novoY*novoPitch];	//cria nova imagem
+
+	GLubyte *dst=p;			//temporario pra copia
+	GLubyte *org=pixels;	//temporario pra copia
+	for(int y=0;y<h;y++)	//copia linha por linha
+	{
+		memcpy(dst,org,pitch);
+		org+=pitch;
+		dst+=novoPitch;
+	}
+
+	//atualiza vars ...
+	delete[] pixels;
+	pixels=p;
+	w=novoX;
+	h=novoY;
+	pitch=novoPitch;
+
+	#ifdef DEBUG
+		printf("Resizada pra %d*%d,%d\n",w,h,pitch);
+	#endif
+	return 0;
+}
---
+++ include/glImagem.h
@@ -0,0 +1,57 @@
+/*
+	MBS-Util
+	Copyright (C) 2006 - Mauricio Bieze Stefani
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef GLIMAGEM_H
+#define GLIMAGEM_H
+#include <gl\gl.h>
+#include "geometria.h"
+
+class glCor
+{
+public:
+	unsigned char r,g,b,a;
+	glCor(unsigned char red=0, unsigned char green=0, unsigned char blue=0, unsigned char alpha=255)
+		: r(red), g(green), b(blue), a(alpha) {}
+	unsigned char *ubv()
+		{ return &r; }
+};
+
+class glImagem
+{
+	public:
+		GLsizei w;		//width: largura em pixels
+		GLsizei h;		//height: altura em pixels
+		GLenum format; 	//formato dos pixels (GL_RGB ou GL_RGBA)
+		GLubyte *pixels;//sem comentarios ;-)
+
+		GLuint glId;	//bind de textura
+		int pitch;		//tamanho de cada linha em bytes
+
+		glImagem();
+		virtual ~glImagem();
+
+		int load(const char *arquivo);	//SDL_image
+		int desenha() const;
+		int bind();		//cria um ID pra textura e carrega na mem de video
+
+		int resize();	//redimensiona imagens pra tamanhos potencia de 2
+		Vetor2D ratio;	//proporcao pra ser usada com glTexCoord2f
+};
+
+#endif // GLIMAGEM_H
